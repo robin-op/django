d5c4cbd6b9aa0225c4fa3ef423d672b15ca75243
robin-op <hans.arancibia@live.com>
2019-11-16 22:31:32 -0300

    hoa

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import include, url
+from rest_framework import routers
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    path(r'personajes', include('personajes.urls')),
-    path(r'cuentas', include('accounts.urls')),
+    url('admin/', admin.site.urls),
+    url('', include('blog.urls')),
+    url(r'personajes/', include('personajes.urls')),
+    url(r'cuentas/', include('accounts.urls')),
 
 ]
